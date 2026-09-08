3071a29204a43a42d34a95bc2c9f8b1c56676781
Homma-0514 <82586481+Homma-0514@users.noreply.github.com>
2026-09-08 15:32:03 +0900

    Update app.js

--- guchan-camera/app.js
+++ guchan-camera/app.js
@@ -1,17 +1,18 @@
+alert("JS読み込み成功");
+
 const video =
 document.getElementById("video");
 
 navigator.mediaDevices
 .getUserMedia({
-
-video:{
-facingMode:"user"
-}
-
+  video:{
+    facingMode:"user"
+  }
 })
-
 .then(stream=>{
-
-video.srcObject = stream;
-
+  alert("カメラ取得成功");
+  video.srcObject = stream;
+})
+.catch(err=>{
+  alert(err.message);
 });
